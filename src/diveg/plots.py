25907c47de0451d42e58f9e24ec0c2b84d82628a
Joachim Mortensen <joamo@sdfe.dk>
2021-06-08 14:41:19 +0200

    maint: Run code through black.

--- src/diveg/plots.py
+++ src/diveg/plots.py
@@ -5,7 +5,8 @@ from typing import (
 
 import matplotlib as mpl
 from matplotlib import style
-style.use('bmh')
+
+style.use("bmh")
 from matplotlib import colors
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -27,20 +28,20 @@ def plot_points_wih_grid(
 
     """
     fig, ax = plt.subplots(figsize=(20, 20))
-    points.plot(ax=ax, markersize=.1, column=column, legend=True)
-    grid.plot(ax=ax, facecolor='none', edgecolor='#cccccc')
+    points.plot(ax=ax, markersize=0.1, column=column, legend=True)
+    grid.plot(ax=ax, facecolor="none", edgecolor="#cccccc")
     fig.tight_layout()
-    plt.savefig(f'{fname}.pdf')
-    plt.savefig(f'{fname}.png')
+    plt.savefig(f"{fname}.pdf")
+    plt.savefig(f"{fname}.png")
 
 
 def plot_ecdfs(points: gpd.GeoSeries, fname: Union[str, pathlib.Path]) -> None:
     """
     Product: ECDF for each cell to see how each cell's data compares, and if there are outliers.
-    
+
     """
     fig, ax = plt.subplots(figsize=(20, 10))
     for (ix, row) in points.iteritems():
-        sns.ecdfplot(row, ax=ax, color='#ff0000', alpha=.1)
+        sns.ecdfplot(row, ax=ax, color="#ff0000", alpha=0.1)
     fig.tight_layout()
-    plt.savefig(f'{fname}.pdf')
+    plt.savefig(f"{fname}.pdf")
